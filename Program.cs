e8be43351ea97c278b74920b1cc6e6c11bf4c9ea
Kirill Yunyshev <ukv@inbox.ru>
2023-06-22 03:15:30 +0300

    minor changes

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
-﻿using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Text.Json;
+﻿using System.Text.Json;
 using Models;
 
 // 1. Generation
-Console.WriteLine(GC.GetTotalMemory(true));
-
 var generator = new PersonGenerator();
-
 List<Person> persons = generator.GeneratePersons(10000).ToList<Person>();
-Console.WriteLine(GC.GetTotalMemory(true));
 
 // 2. Serialization and 3. Write to file
 var serializerOptions = new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-// List<string> serializablePersons = persons.Select(person => JsonSerializer.Serialize(person, serializerOptions)).ToList();
-Console.WriteLine(GC.GetTotalMemory(true));
 
 string fileName = "Persons.json";
 using (FileStream createStream = File.Create(fileName))
@@ -23,10 +15,10 @@ using (FileStream createStream = File.Create(fileName))
 }
 
 // 4. Clear data in memory
+Console.WriteLine(GC.GetTotalMemory(true));
 // TODO: сделать это правильно
 persons.Clear();
 GC.Collect();
-
 Console.WriteLine(GC.GetTotalMemory(true));
 
 // 5. Read from file
